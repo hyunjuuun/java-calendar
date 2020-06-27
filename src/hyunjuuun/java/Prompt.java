00d943e2d85d30b61364db8804a98f3b026af7e5
Hyunjun8612 <hyunjun86120@naver.com>
2020-06-27 22:27:27 +0900

    가상의 달력 출력

---
+++ src/hyunjuuun/java/Prompt.java
@@ -0,0 +1,38 @@
+package hyunjuuun.java;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	private final static String PROMPT = "cal>";
+
+	public void runPrompt() {
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		int month = 1;
+		while (true) {
+			System.out.println("몇 월을 출력할지 입력해주세요. ");
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if (month == -1) {
+				break;
+			}
+			if (month > 12|| month<1) {
+				continue;
+			}
+			cal.printCalendar(2020, month);
+		}
+
+		System.out.println("Bye~");
+		scanner.close();
+
+	}
+
+	public static void main(String[] args) {
+		// 셀 실행
+		Prompt p = new Prompt();
+		p.runPrompt();
+	}
+
+}
